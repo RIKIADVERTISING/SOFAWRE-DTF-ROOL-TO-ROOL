048057c657befb1644a254390f97d633ee9ec1df
RIKI YULIANA <74502177+RIKIADVERTISING@users.noreply.github.com>
2021-06-23 19:09:09 +0700

    Add files via upload

---
+++ v 1/WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.bt_conek = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.pb_conek = New System.Windows.Forms.PictureBox()
+        Me.tb_com = New System.Windows.Forms.TextBox()
+        Me.tb_baudre = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.pb_conek, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'bt_conek
+        '
+        Me.bt_conek.Location = New System.Drawing.Point(90, 38)
+        Me.bt_conek.Name = "bt_conek"
+        Me.bt_conek.Size = New System.Drawing.Size(178, 28)
+        Me.bt_conek.TabIndex = 0
+        Me.bt_conek.Text = "CONECTION"
+        Me.bt_conek.UseVisualStyleBackColor = True
+        '
+        'pb_conek
+        '
+        Me.pb_conek.BackColor = System.Drawing.Color.Red
+        Me.pb_conek.Location = New System.Drawing.Point(15, 38)
+        Me.pb_conek.Name = "pb_conek"
+        Me.pb_conek.Size = New System.Drawing.Size(56, 27)
+        Me.pb_conek.TabIndex = 1
+        Me.pb_conek.TabStop = False
+        '
+        'tb_com
+        '
+        Me.tb_com.Location = New System.Drawing.Point(49, 12)
+        Me.tb_com.Name = "tb_com"
+        Me.tb_com.Size = New System.Drawing.Size(64, 20)
+        Me.tb_com.TabIndex = 2
+        '
+        'tb_baudre
+        '
+        Me.tb_baudre.Location = New System.Drawing.Point(204, 12)
+        Me.tb_baudre.Name = "tb_baudre"
+        Me.tb_baudre.Size = New System.Drawing.Size(64, 20)
+        Me.tb_baudre.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(128, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "BAUDRET"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "COM"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Lime
+        Me.Button1.Location = New System.Drawing.Point(25, 276)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 75)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "UP"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Red
+        Me.Button2.Location = New System.Drawing.Point(193, 276)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 75)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "DOWN"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.Button3.Location = New System.Drawing.Point(112, 165)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 75)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "MENU"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(72, 113)
+        Me.Label3.Name = "Label3"
+        Me.Label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label3.Size = New System.Drawing.Size(128, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "DTF ROOL TO ROOL"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(72, 126)
+        Me.Label4.Name = "Label4"
+        Me.Label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label4.Size = New System.Drawing.Size(116, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "RIKI ADVERTSING"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackColor = System.Drawing.SystemColors.Highlight
+        Me.ClientSize = New System.Drawing.Size(282, 370)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.tb_baudre)
+        Me.Controls.Add(Me.tb_com)
+        Me.Controls.Add(Me.pb_conek)
+        Me.Controls.Add(Me.bt_conek)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "DTF ROOL TO ROOL"
+        Me.TopMost = True
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        CType(Me.pb_conek, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents bt_conek As System.Windows.Forms.Button
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents pb_conek As System.Windows.Forms.PictureBox
+    Friend WithEvents tb_com As System.Windows.Forms.TextBox
+    Friend WithEvents tb_baudre As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Public WithEvents Label3 As System.Windows.Forms.Label
+    Public WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ v 1/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+
+    Private Sub bt_conek_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bt_conek.Click
+        SerialPort1.BaudRate = Val(tb_baudre.Text)
+        SerialPort1.PortName = tb_com.Text
+        Try
+            SerialPort1.Open()
+            If SerialPort1.IsOpen() Then
+                pb_conek.BackColor = Color.Green
+
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("KOMPUTER TIDAK TERKONEKSI KE MODUL DTF ROOL TO ROOL ! ! ! ! ")
+
+        End Try
+      
+
+
+
+    End Sub
+
+    Private Sub tb_baudre_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tb_baudre.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click, Label2.Click
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Label3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label3.Click, Label4.Click
+
+    End Sub
+
+    Private Sub tb_com_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tb_com.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
